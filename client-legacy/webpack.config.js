09ddec0f1f39b13914489747f18db4f1e5349422
Sam Julien <spjulien@gmail.com>
2019-07-08 16:48:57 -0700

    Add polyfills and convert customers table to CE

--- client-legacy/webpack.config.js
+++ client-legacy/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: 'index.html'
     })
   ]
 };
--- ordersystem-ce/src/app/app.module.ts
+++ ordersystem-ce/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { CustomersTableComponent } from './customers-table.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomersTableComponent],
   imports: [BrowserModule],
+  entryComponents: [CustomersTableComponent],
   providers: [],
   bootstrap: []
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(CustomersTableComponent, {
+      injector: this.injector
+    });
+
+    customElements.define('customers-table-ce', el);
+  }
+}
